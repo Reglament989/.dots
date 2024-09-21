8161d97894b939f6b0e129cc30c2109c5e32d965
Danylo O <dee4is.nft@ud.me>
2024-09-21 23:30:11 +0300

    update swww-rand, lock, idle, updated waybar

--- .config/scripts/swww-randomize.sh
+++ .config/scripts/swww-randomize.sh
@@ -5,6 +5,7 @@
 #
 # NOTE: this script is in bash (not posix shell), because the RANDOM variable
 # we use is not defined in posix
+declare -A paths
 
 if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
 	echo "Usage:
@@ -16,6 +17,17 @@ fi
 export SWWW_TRANSITION_FPS=60
 export SWWW_TRANSITION_STEP=90
 
-img=$(find $1 -type f | shuf -n 1)
+# Populate the associative array
+while IFS= read -r file; do
+    filename=$(basename "$file" | awk -F. '{print $1}')
+    paths["$filename"]="$file"
+done < <(find "$1" -type f)
+
+# Show file names to the user with tofi
+selected=$(printf "%s\n" "${!paths[@]}" | tofi --prompt-text "Wallpaper:")
+
+# Get the full path from the selected filename
+img=${paths["$selected"]}
+# img=$(find $1 -type f | awk -F/ '{split($NF, a, "."); print a[1]}' | tofi --prompt-text "Wallpaper:")
 
 swww img --transition-type outer --transition-pos 0.99,0.99 "$img"
